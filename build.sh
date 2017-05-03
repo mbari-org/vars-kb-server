125c54fc0a78fb345ab5823eaffbc2c62baedf15
Brian Schlining <bschlining@gmail.com>
2017-05-03 15:51:22 -0700

    check point

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-sbt pack
-
-docker build -t hohonuuli/vars-kb-server . && \
-  docker push hohonuuli/vars-kb-server
+sbt pack && \
+  docker build -t mbari/vars-kb-server . && \
+  docker push mbari/vars-kb-server
