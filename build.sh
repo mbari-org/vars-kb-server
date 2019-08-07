d5ea2637c475b3b0752f37594e4994e7182124c6
Brian Schlining <bschlining@gmail.com>
2019-08-07 14:43:46 -0700

    check point

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 echo "--- Building vars-kb-server (reminder: run docker login first!!)"
 
 BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
-VCS_REF=`git tag | tail -1`
+VCS_REF=`git tag | sort -V | tail -1`
 
 sbt pack && \
     docker build --build-arg BUILD_DATE=$BUILD_DATE \
