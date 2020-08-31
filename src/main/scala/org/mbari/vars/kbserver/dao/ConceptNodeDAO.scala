085595b14d1f803954af4f2dc5377dc9df3aed3f
Brian Schlining <bschlining@gmail.com>
2020-08-30 20:11:21 -0700

    Added method to find direct children of a concept

--- src/main/scala/org/mbari/vars/kbserver/dao/ConceptNodeDAO.scala
+++ src/main/scala/org/mbari/vars/kbserver/dao/ConceptNodeDAO.scala
@@ -17,19 +17,21 @@
 package org.mbari.vars.kbserver.dao
 
 import org.mbari.vars.kbserver.model.ConceptNode
-import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.{ExecutionContext, Future}
 
 /**
- *
- *
- * @author Brian Schlining
- * @since 2016-11-17T15:49:00
- */
+  *
+  *
+  * @author Brian Schlining
+  * @since 2016-11-17T15:49:00
+  */
 trait ConceptNodeDAO {
 
   def findByName(name: String)(implicit ec: ExecutionContext): Future[Option[ConceptNode]]
 
   def findParent(name: String)(implicit ec: ExecutionContext): Future[Option[ConceptNode]]
+
+  def findChildren(name: String)(implicit ec: ExecutionContext): Future[Seq[ConceptNode]]
 
   def findAllNames()(implicit ec: ExecutionContext): Future[Seq[String]]
 
