ebd5935857b745483f883516c7176836b412776a
Brian Schlining <bschlining@gmail.com>
2022-02-28 13:24:31 -0800

    Adding build script for m1/arm

---
+++ build_m1.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+echo "--- Building vars-kb-server (reminder: run docker login first!!)"
+
+BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
+VCS_REF=`git tag | sort -V | tail -1`
+
+
+sbt pack \
+    && docker buildx build \
+        --platform linux/amd64 \
+        --push \
+        -t mbari/vars-kb-server:${VCS_REF} \
+        -t mbari/vars-kb-server:latest . \
+    && docker buildx build \
+        --platform linux/arm64 \
+        --push \
+        -t mbari/vars-kb-server:${VCS_REF} \
+        -t mbari/vars-kb-server:latest . \
+    && docker buildx build \
+       --platform linux/arm64 \
+       --load \
+       -t mbari/vars-kb-server:${VCS_REF} \
+       -t mbari/vars-kb-server:latest .
+
+
+# docker buildx build \
+#        --platform linux/arm64 \
+#        --load \
+#        -t mbari/vars-kb-server:latest .
