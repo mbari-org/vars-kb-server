a1ac0f71e0c6b7a1884a476003799d647c63d44f
Brian Schlining <bschlining@gmail.com>
2022-03-16 12:38:22 -0700

    Fixed build_mw.sh

--- build_m1.sh
+++ build_m1.sh
@@ -2,19 +2,12 @@
 
 echo "--- Building vars-kb-server (reminder: run docker login first!!)"
 
-BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
 VCS_REF=`git tag | sort -V | tail -1`
 
 
 sbt pack \
-    && docker buildx build \    
-        --platform linux/amd64,linux/arm64 \
-        --push \
+    && docker buildx build --platform linux/amd64,linux/arm64 \
         -t mbari/vars-kb-server:${VCS_REF} \
-        -t mbari/vars-kb-server:latest .
-
-
-# docker buildx build \
-#        --platform linux/arm64 \
-#        --load \
-#        -t mbari/vars-kb-server:latest .
+        -t mbari/vars-kb-server:latest \
+        --push . \
+    && docker pull mbari/vars-kb-server:latest
